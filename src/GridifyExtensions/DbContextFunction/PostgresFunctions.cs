855186679f3101e7e58c7a683aa52d8d70447a42
Haik <haik.asatryan.95@gmail.com>
2024-06-13 15:45:29 +0400

    Stable release

---
+++ src/GridifyExtensions/DbContextFunction/PostgresFunctions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GridifyExtensions.DbContextFunction;
+
+public class PostgresFunctions : DbContext //By inheriting PostgresFunctions you can use the Substr function in your queries
+{
+    protected PostgresFunctions(DbContextOptions options) : base(options)
+    {
+    }
+
+    [DbFunction("substr", null, IsBuiltIn = true)]
+    public static byte[] Substr(byte[] target, int start, int count)
+    {
+        throw new NotImplementedException();
+    }
+}
