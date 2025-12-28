fba9a4aba3b7ddefb9be970f22d1ebd5f5c19e16
Haik <haik.asatryan.95@gmail.com>
2025-12-28 20:57:59 +0400

    small cleanup

--- src/GridifyExtensions/Models/AggregateQueryModel.cs
+++ src/GridifyExtensions/Models/AggregateQueryModel.cs
@@ -3,7 +3,6 @@
 namespace GridifyExtensions.Models;
 
 public class AggregateQueryModel : GridifyQueryModel
-
 {
    public required string PropertyName { get; set; }
    public required AggregateType AggregateType { get; set; }
